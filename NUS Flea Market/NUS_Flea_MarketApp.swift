bcb4555411b481eb55b44a3594728e595f4c0bcc
MaYuehan <myh_qian@163.com>
2023-06-26 01:03:31 +0800

    Initial Commit

---
+++ NUS Flea Market/NUS_Flea_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  NUS_Flea_MarketApp.swift
+//  NUS Flea Market
+//
+//  Created by Yuehan Ma on 26/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct NUS_Flea_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
